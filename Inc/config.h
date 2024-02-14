3a7045ca2b9d116b5acc8b12d9ffc0dc6d9e145c
hoelgi777 <153670304+hoelgi777@users.noreply.github.com>
2024-02-14 12:12:15 +0100

    King-Meter 901U protocol

--- Inc/config.h
+++ Inc/config.h
@@ -97,7 +97,7 @@
 
 //---------------------------------------------------------------------
 //Display settings
-#define DISPLAY_TYPE DISPLAY_TYPE_EBiCS
+#define DISPLAY_TYPE King-Meter 901U protocol
 
 
 //---------------------------------------------------------------------
