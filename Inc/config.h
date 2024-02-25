1b0a72f00d2aaaf2bbfe6f8b1550d3947c088db4
hoelgi777 <153670304+hoelgi777@users.noreply.github.com>
2024-02-25 12:47:34 +0100

    Update config.h

--- Inc/config.h
+++ Inc/config.h
@@ -60,8 +60,8 @@
 //----------------------------------------------------------------------
 //PAS mode settings
 #define DIRDET
-#define FRAC_HIGH 70
-#define FRAC_LOW 42
+#define FRAC_HIGH 57
+#define FRAC_LOW 53
 #define PAS_TIMEOUT 8000
 #define RAMP_END 1600
 
